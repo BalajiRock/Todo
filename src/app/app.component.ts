8c162f61c65ff90ccf8ef9bed45d3114b95cf961
Balaji <balajiagmohan@gmail.com>
2024-01-26 22:12:43 +0530

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Todo List';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "hello madu";
+    // }, 5000);
+  }
 }
